0cc04b04e6df8c4400690d6342a87b6a0c391b8f
Anthony <anthony.lisa0@gmail.com>
2025-01-19 19:21:44 -0800

    anthony changes :)

--- scenes/maps/Map.cs
+++ scenes/maps/Map.cs
@@ -4,25 +4,24 @@ using PirateJam.scenes.player;
 
 namespace PirateJam.scenes.maps;
 
-
 public partial class Map : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		var player = GetNode<Player>("Player");
-		var map = GetNode<TileMapLayer>("Map");
-		var startPostion = GetNode<Marker2D>("StartMarker");
-		player.Start(startPostion.Position);
-		
-		// Example of how to get tile information:
-		Vector2I firstTile = Vector2I.Zero;  // Tile at 0,0
-		var tiledata = map.GetCellTileData(firstTile);
-		var type = tiledata.GetCustomData("type"); // This is the property assigned in the TileSet
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var player = GetNode<Player>("Player");
+        var map = GetNode<TileMapLayer>("Map");
+        var startPostion = GetNode<Marker2D>("StartMarker");
+        player.Move(startPostion.Position);
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+        // Example of how to get tile information:
+        Vector2I firstTile = Vector2I.Zero; // Tile at 0,0
+        var tiledata = map.GetCellTileData(firstTile);
+        var type = tiledata.GetCustomData("type"); // This is the property assigned in the TileSet
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -1,31 +1,69 @@
 using Godot;
 using System;
+using PirateJam.scenes.maps;
 
 namespace PirateJam.scenes.player;
 
 public partial class Player : Area2D
 {
-	private readonly PlayerWeapon _weapon = new PlayerWeapon();
-	private Vector2 _screenSize;
-	private TileMapLayer _tileMap;
-	
-	public PlayerWeapon Weapon { get; set; }
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_screenSize = GetViewportRect().Size;
-		_weapon.WeaponName = "Sword";
-	}
+    private readonly PlayerWeapon _weapon = new PlayerWeapon();
+    private Vector2 _screenSize;
+    private TileMapLayer _tileMap;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public PlayerWeapon Weapon { get; set; }
 
-	public void Start(Vector2 startPositionPosition)
-	{
-		Position = startPositionPosition;
-		GD.Print($"Starting position: {Position}");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _screenSize = GetViewportRect().Size;
+        _weapon.WeaponName = "Sword";
+    }
+
+    public ConvertPosition
+    
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        var moveTo = Position;
+        if (Input.IsActionJustPressed("move_down"))
+        {
+            moveTo.Y += 32;
+        }
+
+        if (Input.IsActionJustPressed("move_up"))
+        {
+            moveTo.Y -= 32;
+        }
+
+        if (Input.IsActionJustPressed("move_left"))
+        {
+            moveTo.X -= 32;
+        }
+
+        if (Input.IsActionJustPressed("move_right"))
+        {
+            moveTo.X += 32;
+        }
+
+        var map = GetParent().GetNode("Map") as TileMapLayer;
+        
+        var tileDataIndex = new Vector2I(Mathf.FloorToInt(moveTo.X - 49),
+            Mathf.FloorToInt(moveTo.Y -49 ));
+        var tiledata = map.GetCellTileData(tileDataIndex);
+        // // var type = tiledata.GetCustomData("type"); // This is the property assigned in the TileSet
+        GD.Print(tileDataIndex);
+        GD.Print(tiledata);
+        
+        // if ( typeof(type == "string")
+        // {
+        //     
+        // }
+        Move(moveTo);
+    }
+
+    public void Move(Vector2 startPositionPosition)
+    {
+        Position = startPositionPosition;
+        GD.Print($"Moving to: {Position}");
+    }
 }
