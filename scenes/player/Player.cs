b6997832e9468b34839c65ff2804c835dba671cb
aoroudjev <a.oroudjev@gmail.com>
2025-01-21 19:21:23 -0800

    Add movement indicators and win check

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -11,6 +11,8 @@ public partial class Player : Area2D
     private AnimatedSprite2D _spriteAnimation;
     private bool _isMoving;
     private Vector2 _globalTargetPosition;
+    private Vector2I _selectedDirection;
+    private Sprite2D _directionSprite;
 
     public override void _PhysicsProcess(double delta)
     {
@@ -18,17 +20,25 @@ public partial class Player : Area2D
 
         if (GlobalPosition != _globalTargetPosition)
         {
-            GlobalPosition = GlobalPosition.MoveToward(_globalTargetPosition, 2);
+            GlobalPosition = GlobalPosition.MoveToward(_globalTargetPosition, 2); //TODO: not hardcoded delta
             return;
+        }
+
+        if (_tileMap.GetCellTileData(_GetCurrentTilePosition()).GetCustomData("type").AsString() == "door")
+        {
+            GD.Print("CUM");
+            // TODO Level Cumplete
         }
         
         _isMoving = false;
+        // TODO: still retains the selected direction, wanted behavior?
     }
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        _weapon = new Bow();
+        _weapon = new Sword();
+        _directionSprite= GetNode<Sprite2D>("Arrow");
         _spriteAnimation = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         _spriteAnimation.Play(_weapon.Animations["idle"]);
         _tileMap = GetParent().GetNode<TileMapLayer>("Map");
@@ -43,23 +53,44 @@ public partial class Player : Area2D
         if (_isMoving) return;
         
         if (Input.IsActionJustPressed("move_down"))
-            MovePlayer(Vector2I.Down);
+            SelectDirection(Vector2I.Down);
         else if (Input.IsActionJustPressed("move_up")) 
-            MovePlayer(Vector2I.Up);
+            SelectDirection(Vector2I.Up);
         else if (Input.IsActionJustPressed("move_left")) 
-            MovePlayer(Vector2I.Left);
+            SelectDirection(Vector2I.Left);
         else if (Input.IsActionJustPressed("move_right")) 
-            MovePlayer(Vector2I.Right);
+            SelectDirection(Vector2I.Right);
+        else if (Input.IsActionJustPressed("move_confirm"))
+            MovePlayer(_selectedDirection);
     }
 
+    public void SelectDirection(Vector2I direction)
+    {
+        _selectedDirection = direction;
+        _directionSprite.Position = direction;
+        _directionSprite.Rotation = Mathf.Atan2(direction.Y, direction.X) ;
+        _directionSprite.Visible = true;
+        
+        Vector2I currTile = _GetCurrentTilePosition();
+        _directionSprite.GlobalPosition = _tileMap.MapToLocal(currTile + direction);
+    }
+    
+    
     public void MovePlayer(Vector2I direction)
     {
         _isMoving = true;
-        Vector2I currTile = _tileMap.LocalToMap(GlobalPosition);
+        _directionSprite.Visible = false;
+        
+        Vector2I currTile = _GetCurrentTilePosition();
         Vector2I targetPosition = _weapon.GetTargetPosition(direction, _tileMap, currTile);
         
         // Move in linear format
         _globalTargetPosition = _tileMap.MapToLocal(targetPosition);
+    }
+
+    private Vector2I _GetCurrentTilePosition()
+    {
+        return _tileMap.LocalToMap(GlobalPosition);
     }
 
     
